f1a8dff0debfe8c3159713c30833c208674fb51e
corey-mitchell <corey69_96@yahoo.com>
2018-08-09 22:34:45 -0500

    set default movie for movie-this function. Officially done with that command.

--- liri.js
+++ liri.js
@@ -2,8 +2,8 @@
 require("dotenv").config();
 
 // Imports keys.js
-let keys = require("./keys.js");
-// import {spotify} from "./keys.js";  <=== ES6 Version uses import, not currently supported by node
+require("./keys.js");
+// import {spotify} from "./keys.js";  <=== ES6 Version uses import, not currently supported by node. Just something I learned that I am leaving in the code for later use.
 
 // Other Var ***Need to set up***
 // let fs = require("fs");
@@ -11,16 +11,15 @@ let keys = require("./keys.js");
 // let twitter = new Twitter(keys.twitter);
 
 
-// Defines command so that I don't have to type process.argv all day
+// Defines command so that I don't have to type process.argv[2] all day
 let command = process.argv[2];
 
-// Movie-This Command
-if(command === "movie-this") {
-    let movieTitle = process.argv[3]
+
+let movieThis = (title='Mr. Nobody') => {
     let omdbApi = require("omdb-client");
     let params = {
         apiKey: process.env.OMDB_KEY,
-        title: movieTitle
+        title: title
     }
 
     omdbApi.get(params, (err, movies) => {
@@ -28,17 +27,14 @@ if(command === "movie-this") {
             return console.log(err);
         }
 
-        // If no movie is entered this default it to Mr. Nobody as per homework instructions
-        // ***Need to fix***
-        if (process.argv[3] === -1) {
-            title = "Mr. Nobody"
-            return console.log (movies.Title)
-        }
-
         // console.logs Out Movie Info
-        console.log(`Title: ${movies.Title}\nYear: ${movies.Year}\nIMDb Rating: ${movies.imdbRating}/10\nCountry Created: ${movies.Country}\nLaunguage: ${movies.Language}\nActors: ${movies.Actors}\nPlot: ${movies.Plot}`)
-
+        console.log(`Title: ${movies.Title}\nYear: ${movies.Year}\nIMDb Rating: ${movies.imdbRating}/10\nCountry Created: ${movies.Country}\nLaunguage: ${movies.Language}\nActors: ${movies.Actors}\nPlot: ${movies.Plot}`);
     })
+}
+
+// Movie-This Command
+if(command === "movie-this") {
+    movieThis(process.argv[3]);
 }
 // else if(command === "my-tweets") {
 
@@ -46,6 +42,9 @@ if(command === "movie-this") {
 
 // }else if (command === "do-what-it-says") {
 
-// }else {
-//     console.log("No Command Given")
 // }
+
+// Let's user know that they are missing a command
+else {
+    console.log("No Command Given")
+}
