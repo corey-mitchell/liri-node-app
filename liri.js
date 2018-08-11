a9a60d221bf60e85f2211d2123846c3dce8043dd
corey-mitchell <corey69_96@yahoo.com>
2018-08-11 16:39:06 -0500

    made initial my-tweet function. but it doesn't work yet :/

--- liri.js
+++ liri.js
@@ -1,19 +1,19 @@
 // Reads and sets any environment variables with the dotenv package
 require("dotenv").config();
 
+// Allows File Reading and Writing
+const fs = require("fs");
+
 // Imports keys.js
 const keys = require("./keys.js");
 // import {spotify} from "./keys.js";  <=== ES6 Version uses import, not currently supported by node. Just something I learned that I am leaving in the code for later use.
 
-const Spotify = require('node-spotify-api');
-
-// Other Var ***Need to set up***
-// let fs = require("fs");
-// let twitter = new Twitter(keys.twitter);
-
+// Initializes Spotify and Twitter node APIs
+const spotifyAPI = require('node-spotify-api');
+const twitterAPI = require('node-twitter-api');
 
 // Defines command so that I don't have to type process.argv[2] all day
-let command = process.argv[2];
+const command = process.argv[2];
 
 // Function for defining movie-this command
 const movieThis = (title='Mr. Nobody') => {
@@ -21,7 +21,7 @@ const movieThis = (title='Mr. Nobody') => {
     const params = {
         apiKey: process.env.OMDB_KEY,
         title: title
-    }
+    };
 
     omdbApi.get(params, (err, movies) => {
         if (err) {
@@ -30,20 +30,33 @@ const movieThis = (title='Mr. Nobody') => {
 
         // console.logs Out Movie Info
         console.log(`Title: ${movies.Title}\nYear: ${movies.Year}\nIMDb Rating: ${movies.imdbRating}/10\nCountry Created: ${movies.Country}\nLaunguage: ${movies.Language}\nActors: ${movies.Actors}\nPlot: ${movies.Plot}`);
-    })
-}
+    });
+};
 
 // Function for defining spotify-this command
+// Use format 'song, artist' to help find more specific tracks. If you pass just the song title the response will be top spotify track by that name.
+// If the artist is not on spotify then your response will not be what you expect. Example the band 'Tool'
 const spotifyThis = (song='The Sign, Ace of Base') => {
-    const spotify = new Spotify(keys.spotify);
+    const spotify = new spotifyAPI(keys.spotify);
     spotify.search({type: 'track', query: song, limit: 5}, (err, data) => {
         if (err) {
             return console.log(err);
         };
         const songInfo = data.tracks.items[0];
-        // console.log(songInfo);
+        // console.logs out song info;
         console.log(`Artist: ${songInfo.artists[0].name}\nSong: ${songInfo.name}\nLink to song: ${songInfo.external_urls.spotify}\nAlbum: ${songInfo.album.name}`);
-    })
+    });
+};
+
+const myTweets = () => {
+    const twitter = new twitterAPI(keys.twitter);
+    twitter.statuses('show', {limit: 20}, process.env.TWITTER_ACCESS_TOKEN_KEY, process.env.TWITTER_ACCESS_TOKEN_SECRET, (err,data) => {
+        if (err) {
+            return console.log(err);
+        };
+
+        console.log(data);
+    });
 };
 
 // movie-this command
@@ -52,11 +65,12 @@ if(command === "movie-this") {
 }
 
 // my-tweets command
-// else if(command === "my-tweets") {
-
-// }
+else if(command === "my-tweets") {
+    myTweets();
+}
 
 // spotify-this-song command
+// Use format 'song, artist' to help find more specific tracks. If you pass just the song title the response will be top spotify track by that name.
 else if(command === "spotify-this-song") {
     spotifyThis(process.argv[3]);
 }
@@ -68,5 +82,5 @@ else if(command === "spotify-this-song") {
 
 // Let's user know that they are missing a command
 else {
-    console.log("No Command Given")
+    console.log("Missing or invalid command");
 };
